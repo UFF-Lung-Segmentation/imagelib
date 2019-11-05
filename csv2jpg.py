a66d85f3c9bae0954c8a56a6453da2df903cc99d
Paulo Lacerda <pclacerda@gmail.com>
2019-11-05 12:17:47 -0300

    carga incial

--- csv2jpg.py
+++ csv2jpg.py
@@ -3,11 +3,10 @@ import os
 import imageio
 import util
 
-# [substituir nesta seção diretórios de entrada e saída]
-segmented_folder  = util.config["output_path"]
+input_folder  = util.config["segmentation_path"]
 output_folder = util.config["jpg_path"]
 
-input_folder_enconded = os.fsencode(segmented_folder)
+input_folder_enconded = os.fsencode(input_folder)
 output_folder_enconded = os.fsencode(output_folder)
 input_files = os.listdir(input_folder_enconded)
 input_files.sort()
@@ -24,7 +23,7 @@ for filename in input_files:
     if (input_filename.find(".csv") > 0) or (input_filename.find(".txt") > 0):
 
         # carrega a imagem
-        input_filepath = "{}/{}".format(segmented_folder, os.fsdecode(input_filename))
+        input_filepath = "{}/{}".format(input_folder, os.fsdecode(input_filename))
         image = np.loadtxt(open(input_filepath, "rb"), delimiter=",")
         image = image.astype("int16")
         image = np.clip(image, -1024, 1024)
--- dicom2csv.py
+++ dicom2csv.py
@@ -3,9 +3,8 @@ import os
 import pydicom
 import util
 
-# [substituir nesta seção diretórios de entrada e saída]
 input_path  = util.config["dicom_path"]
-csv_path = util.config["input_path"]
+output_path = util.config["csv_path"]
 
 input_folder = os.fsencode(input_path)
 files = os.listdir(input_folder)
@@ -23,7 +22,7 @@ for file in files:
     image = m * ds.pixel_array + b
 
     # grava arquivo csv
-    output_filename = "{}/{}{}".format(csv_path, os.fsdecode(file)[:-4], ".txt")
+    output_filename = "{}/{}{}".format(output_path, os.fsdecode(file)[:-4], ".txt")
     print(output_filename)
     image = numpy.int16(image)
     numpy.savetxt(output_filename, image, fmt="%d", delimiter=",")
